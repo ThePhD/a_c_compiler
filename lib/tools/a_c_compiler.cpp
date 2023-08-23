4330ca7c94ac492f7e9d5628415b66caa4a43851
Asher Mancinelli <ashermancinelli@gmail.com>
2023-08-23 09:49:36 -0700

    Check file type and existence

--- lib/tools/a_c_compiler.cpp
+++ lib/tools/a_c_compiler.cpp
@@ -1,35 +1,15 @@
+#include <filesystem>
 #include <iomanip>
 #include <iostream>
 #include <string>
 #include <string_view>
 #include <vector>
 
+namespace fs = std::filesystem;
+
 static struct {
-  std::vector<std::string> positional_args;
+  std::vector<fs::path> positional_args;
 } cli_opts;
-
-bool parse_args(const std::vector<std::string> &args) {
-  auto it = args.begin();
-  while (it != args.end()) {
-    /* parse flags */
-    if (*it == "-h" or *it == "--help") {
-      return false;
-    }
-
-    /* parse positional args */
-    else {
-      cli_opts.positional_args.push_back(*it);
-    }
-    it++;
-  }
-
-  /* Check validity of command line args. At this point, we only check that
-   * we have exatly one positional arg, the input source file. */
-  if (cli_opts.positional_args.size() != 1) {
-    return false;
-  }
-  return true;
-}
 
 int help(std::string_view exe) {
   const auto help_flag = [](const char *f_short, const char *f_long, const char *help) {
@@ -44,10 +24,48 @@ int help(std::string_view exe) {
   return EXIT_FAILURE;
 }
 
+#define ARGPARSEASSERT(cond, msg)                                                                  \
+  if (!(cond)) {                                                                                   \
+    std::cout << "\n" << msg << "\n\n";                                                            \
+    return false;                                                                                  \
+  }
+
+bool parse_args(const std::string &exe, const std::vector<std::string> &args) {
+  auto it = args.begin();
+  while (it != args.end()) {
+    /* parse flags */
+    if (*it == "-h" or *it == "--help") {
+      return false;
+    }
+
+    /* parse positional args */
+    else {
+      cli_opts.positional_args.emplace_back(*it);
+    }
+    it++;
+  }
+
+  /* Check validity of command line args. */
+
+  /* Check that we have exatly one positional arg, the input source file. */
+  ARGPARSEASSERT(cli_opts.positional_args.size() == 1, "We only handle one source file atm");
+
+  /* Check that all positional args are files that exist */
+  for (auto const &fpath : cli_opts.positional_args) {
+    ARGPARSEASSERT(fs::exists(fpath), "Expected source file to exist!");
+    const auto ty = fs::status(fpath).type();
+    using ft = fs::file_type;
+    ARGPARSEASSERT(ty == ft::regular or ty == ft::symlink,
+                   "Expected source file to be a regular file or a symlink!");
+  }
+
+  return true;
+}
+
 int main(int argc, char **argv) {
   std::string exe(argv[0]);
   std::vector<std::string> args(argv + 1, argv + argc);
-  if (!parse_args(args)) {
+  if (!parse_args(exe, args)) {
     return help(exe);
   }
   return EXIT_SUCCESS;
