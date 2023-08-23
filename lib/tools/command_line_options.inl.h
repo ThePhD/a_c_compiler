4fdb1dd667b3fc6bc69ee9c109cd29b493a352b8
Asher Mancinelli <ashermancinelli@gmail.com>
2023-08-23 13:11:30 -0700

    Put exe in top-level build dir
    Also refine debug options so all longer option names
    start with -f.

--- lib/tools/command_line_options.inl.h
+++ lib/tools/command_line_options.inl.h
@@ -6,7 +6,7 @@
  */
 FLAG(help, false, "-h", "--help", "Print help message")
 FLAG(verbose, false, "-v", "--verbose", "Display extra information from the driver")
-FLAG(debug_lexer, false, "-L", "--debug-lexer", "Dump tokens after lexing phase")
+FLAG(debug_lexer, false, "-L", "-fdebug-lexer", "Dump tokens after lexing phase")
 #endif
 
 #ifdef OPTION
